b64e419d67e77f69b19ff88c7e7fb37945a7e2ab
Kalvis Apsitis <kalvis.apsitis@gmail.com>
2024-01-02 09:12:59 +0000

    perfect 25

--- konstrukcijas/perfect_special/extreme_all.sh
+++ konstrukcijas/perfect_special/extreme_all.sh
@@ -2,14 +2,11 @@
 
 echo "Processing ${1}-polyiamonds:"
 
-/Users/kapsitis/opt/anaconda3/bin/python perfect_extremes_parallel.py perfect "$1" area
-/Users/kapsitis/opt/anaconda3/bin/python best_prefixes.py perfect "$1" area
-/Users/kapsitis/opt/anaconda3/bin/python perfect_extremes_parallel.py perfect "$1" diameter
-/Users/kapsitis/opt/anaconda3/bin/python best_prefixes.py perfect "$1" diameter
-/Users/kapsitis/opt/anaconda3/bin/python perfect_extremes_parallel.py perfect "$1" dimension
-/Users/kapsitis/opt/anaconda3/bin/python best_prefixes.py perfect "$1" dimension
-/Users/kapsitis/opt/anaconda3/bin/python perfect_extremes_parallel.py perfect "$1" width
-/Users/kapsitis/opt/anaconda3/bin/python best_prefixes.py perfect "$1" width
-
-
-
+python perfect_extremes_parallel.py perfect "$1" area
+python best_prefixes.py perfect "$1" area
+python perfect_extremes_parallel.py perfect "$1" diameter
+python best_prefixes.py perfect "$1" diameter
+python perfect_extremes_parallel.py perfect "$1" dimension
+python best_prefixes.py perfect "$1" dimension
+python perfect_extremes_parallel.py perfect "$1" width
+python best_prefixes.py perfect "$1" width
